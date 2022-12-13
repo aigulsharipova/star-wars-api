8b2724a54c41a712028fbbfac900a9914425dbd1
Aigul <sharipova.aig@gmail.com>
2022-12-12 22:02:50 -0500

    api star-wars

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-async function getStar() {
-const res = await fetch('https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people/1/');
- console.log(res);
+const api = {
+  endpoint: "https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people/1/"
 }
